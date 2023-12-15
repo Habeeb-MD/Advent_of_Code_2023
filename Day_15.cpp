d8f3382a65aa67c2dfad4fea78da095f77de1d45
Habeeb-MD <msub989@gmail.com>
2023-12-15 17:17:32 +0530

    updated solution for day 15

--- Day_15.cpp
+++ Day_15.cpp
@@ -1,9 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<string> grid;
-int n, m;
-
 std::vector<std::string> split(const std::string &s, char delim)
 {
     std::vector<std::string> result;
